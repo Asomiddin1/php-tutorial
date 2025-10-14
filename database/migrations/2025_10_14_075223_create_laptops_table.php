496227b3e6a2894dd686ccb29118f2826e0a3783
Asomiddin <asomiddin320@gmail.com>
2025-10-14 12:53:28 +0500

    8-topshiriq

---
+++ database/migrations/2025_10_14_075223_create_laptops_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('laptops', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand'); // Masalan: Asus, HP, Lenovo
+            $table->decimal('price', 10, 2); // Masalan: 1200.50
+            $table->string('color'); // Masalan: black, silver
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laptops');
+    }
+};
