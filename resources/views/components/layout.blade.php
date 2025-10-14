cccf1f9146c343519fb4dd5a3b6e973378a01871
Asomiddin <asomiddin320@gmail.com>
2025-10-14 10:59:34 +0500

    5-topshiriq

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,16 +8,15 @@
   </head>
 </head>
 <body>
-    <nav class="flex justify-between bg-gray-500 px-8 py-4 text-white">
-
-        <h1 class='text-[22px] font-semibold '><a href="/">Laravel</a></h1>
-      <ul class="flex space-x-4 ">
-        <li><a href="/about">About</a></li>
-        <li><a href="/students">Students</a></li>
-        <li><a href="/users">Users</a></li>
-        <li><a href="/jobs">Jobs</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
+    <nav class="flex items-center justify-between py-4 px-8 bg-gray-700 text-white">
+        <h1 class="text-[22px] font-semibold"><a href="/">Larevel</a></h1>
+       <ul class="flex space-x-4">
+        <li class="{{ request()->is('about') ? ' bg-slate-800 text-white px-4 py-2 rounded-md' : 'px-4 py-2 hover:text-white' }}"><a href="/about">About</a></li>
+        <li  class="{{ request()->is('students') ? 'bg-slate-800 text-white px-4 py-2 rounded-md' : 'px-4 py-2 hover:text-white' }}"><a href="/students">Students</a></li>
+        <li  class="{{ request()->is('users') ? 'bg-slate-800 text-white px-4 py-2 rounded-md' : 'px-4 py-2 hover:text-white' }}"><a href="/users">Users</a></li>
+        <li  class="{{ request()->is('jobs') ? 'bg-slate-800 text-white px-4 py-2 rounded-md' : 'px-4 py-2 hover:text-white' }}"><a href="/jobs">Jobs</a></li>
+        <li  class="{{ request()->is('contact') ? 'bg-slate-800 text-white px-4 py-2 rounded-md' : 'px-4 py-2 hover:text-white' }}"><a href="/contact">Contact</a></li>
+       </ul>
     </nav>
     <div class="bg-gray-200 p-4 text-start text-[20px] font-semibold">
         {{ $heading }}
